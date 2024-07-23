9bc70049c3f053a82153ebf5b8440d6365f02d8b
Claudia Robles <claudia.rd.pro@gmail.com>
2024-07-23 16:07:55 +0200

    ejercicio string

--- Basic/07-strings-exercises.js
+++ Basic/07-strings-exercises.js
@@ -1,20 +1,48 @@
 
 // 1. Concatena dos cadenas de texto
+        let n = "hola"
+        let h = " me alegro!"
+        let resultado = n + h
+        console.log(resultado)
+
 
 // 2. Muestra la longitud de una cadena de texto
 
+        console.log(resultado.length)
+
 // 3. Muestra el primer y último carácter de un string
+
+        console.log(resultado[0])
+        console.log(resultado[resultado.length - 1])
 
 // 4. Convierte a mayúsculas y minúsculas un string
 
+        console.log(resultado.toUpperCase())
+        console.log(resultado.toLowerCase())
+
 // 5. Crea una cadena de texto en varias líneas
+
+        let = `texto
+        en vartias lineas`
 
 // 6. Interpola el valor de una variable en un string
 
+    console.log(`interpolacion: ${resultado}`)
+
 // 7. Reemplaza todos los espacios en blanco de un string por guiones
+
+    console.log(resultado.replace(" ","-"))
 
 // 8. Comprueba si una cadena de texto contiene una palabra concreta
 
+    console.log(resultado.includes("hola"))
+
 // 9. Comprueba si dos strings son iguales
 
+let isEquals = (n == h)
+console.log(isEquals)
+
 // 10. Comprueba si dos strings tienen la misma longitud
+
+let isEqualsLength = (resultado.length == n.length)
+console.log(isEqualsLength)
